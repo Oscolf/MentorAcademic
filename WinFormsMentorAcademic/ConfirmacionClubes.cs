e660ebfa5a48b36758071cee2da75e53f4c938bb
Oscar Vázquez <oscarvr.110503@gmail.com>
2025-07-29 14:20:39 -0600

    Correción en el botón para mandar registro al Club

--- WinFormsMentorAcademic/ConfirmacionClubes.cs
+++ WinFormsMentorAcademic/ConfirmacionClubes.cs
@@ -4,10 +4,12 @@ namespace WinFormsMentorAcademic;
 
 public partial class ConfirmacionClubes : Form
 {
-    public ConfirmacionClubes(String clubName)
+    public ConfirmacionClubes(String clubName, string userMatr)
     {
         InitializeComponent();
         lbl_clubSel.Text = clubName;
+        lbl_matr.Text = userMatr;
+
     }
 
     private void btn_send_Click(object sender, EventArgs e)
@@ -15,7 +17,11 @@ public partial class ConfirmacionClubes : Form
         SqlQueries clubesQuery = new SqlQueries("server=localhost; port=3306; " +
                                         "database=mentor_academic; user=root;");
 
-        string cmd = $"UPDATE alumnos SET club_pertenece = '{lbl_clubSel.Text}' ";
-        //$"WHERE matricula = '{User.Matricula}';";
+        string cmd = $"UPDATE alumnos SET club_pertenece = '{lbl_clubSel.Text}'" +
+        $"WHERE matricula = '{lbl_matr.Text}';";
+
+        clubesQuery.GetCommand_and_ExecuteNonQuery(cmd);
+
+        MessageBox.Show("Se ha registrado correctamente en el club");
     }
 }
